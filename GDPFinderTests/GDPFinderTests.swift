47e6a6b8841ec162648cd7d00c3520f4bad3759a
nafisar <ios.nafisa@gmail.com>
2018-01-09 13:35:10 +1100

    Initial Commit

---
+++ GDPFinderTests/GDPFinderTests.swift
@@ -0,0 +1,36 @@
+//
+//  GDPFinderTests.swift
+//  GDPFinderTests
+//
+//  Created by Nafisa Rahman on 1/9/18.
+//  Copyright © 2018 com.nafisa. All rights reserved.
+//
+
+import XCTest
+@testable import GDPFinder
+
+class GDPFinderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
